c46493a2b564e133ffeb48164b7ec76eae848b07
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-08 09:13:41 +0000

    einige Tests durchgeführt

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -13,9 +13,34 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); //Ausgabe
-firstName = "IchBins"; // Wertzuweisung | assignment
-console.log(firstName); //Ausgabe
-let familyName = "Nicht"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin IchBins Nicht!
+//let firstName; // Deklaration (Definition)
+//console.log(firstName); //Ausgabe
+//firstName = "IchBins"; // Wertzuweisung | assignment
+//console.log(firstName); //Ausgabe
+//let familyName = "Nicht"; // Deklaration + Wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin IchBins Nicht!
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+// let test;
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("----------------------------------");
+
+// test = "hi"; // ist ein string - ´hi´ - kann auch so stehen, besser ist definitiv mit ""
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("----------------------------------");
+
+// test = 2; // number
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("----------------------------------");
+
+// test = true; //
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("----------------------------------");
