f1d08871d369b5d7f75350d9089cce5fafa572a5
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-08 08:34:35 +0000

    Deklaration und Wertzuweisung

---
+++ src/02_variablen.js
@@ -0,0 +1,21 @@
+// Zeilenkommentar | Strg + #
+
+/**
+ * 
+ * Block-Kommentar
+ * 
+ */
+
+/***** Variablen 01 *****/
+
+//console.log("Hi");
+//console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); //Ausgabe
+firstName = "IchBins"; // Wertzuweisung | assignment
+console.log(firstName); //Ausgabe
+let familyName = "Nicht"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin IchBins Nicht!
