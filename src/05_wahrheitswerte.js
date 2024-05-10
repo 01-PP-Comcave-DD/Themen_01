9a04486964bb24b14593256fe404e2389027e8d3
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-10 08:56:58 +0000

    Wahrheitswerte ++

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -1,17 +1,29 @@
 
 /* Berechnung Alter */
 
-// Deklaration
+// Deklaration (Ausgangswerte)
 let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
 // Berechnung Alter
-let year = 2024;
+// Rechenweg: 2024/2024 - 1980/1990 = 44/34
+let date = new Date(); // Systemfunktion Datum
+let year = date.getFullYear(); // Daten liefern lassen!!
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+//console.log("Datum: " + date);
+
+
+//Tests
+let isJohnOlder, isJohnEqual; // Is John older? --> y/n --> true/false
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
 
