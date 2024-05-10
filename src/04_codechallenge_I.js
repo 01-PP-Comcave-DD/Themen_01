52a70c3d7c5d8721ff4fbb650bde9f3ec4279ae4
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-10 07:32:52 +0000

    eine weitere Lösung in der Codechallenge

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -11,8 +11,13 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 
 const prompt = require('prompt-sync')({sigint: true});
 // Zahleneingabe über eine Prompt-Anforderung
-let zahl1 = parseFloat(prompt("Gib die erste Zahl ein:"));  //Fragen was parseFloat bedeutet
-let zahl2 = parseFloat(prompt("Gib die zweite Zahl ein:"));
+//parsefloat() --> wandelt einen String in eine Dezimalzahl (eine Zahl mit Dezimalstellen mit Nachkommazahl) um
+let zahl1 = parseFloat(prompt("Gib die erste Zahl ein: "));
+let zahl2 = parseFloat(prompt("Gib die zweite Zahl ein: "));
+
+//parseInt() --> wandelt einen String in eine Dezimalzahl (eine Ganzzahl ohne Nachkommazahl) um
+//let zahl1 = parseInt(prompt("Gib die erste Zahl ein: "));
+//let zahl2 = parseInt(prompt("Gib die zweite Zahl ein: "));
 
 // Berechnung der Summe
 let summe = zahl1 + zahl2;
