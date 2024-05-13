8765ab57b402a4aa17b75684fb60234d8f93b49a
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-13 09:45:28 +0000

    Funktionen 02c

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -29,10 +29,23 @@ function outputNames() {
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-outputNames2("Rene");          //Argumente --> Daten für die Parameter
-outputNames2("Patrick");
-outputNames2();      //<---- ohne Inhalt, deswegen der fehler undefined
+// outputNames2("Rene");          //Argumente --> Daten für die Parameter
+// outputNames2("Patrick");
+// outputNames2();      //<---- ohne Inhalt, deswegen der fehler undefined
 
 function outputNames2(firstname) {    //Parameter
     console.log("Hallo, " + firstname + "!");
+}
+
+//--------------------------------------------------------//
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+outputNames3("Rene","heißt anders als du")
+
+const prompt = require('prompt-sync')({sigint: true});
+outputNames3(prompt("Vorname?: "),prompt("Nachname?: "))
+
+function outputNames3(firstname, familyname) {    //Parameter
+    console.log("Hallo, " + firstname + " " + familyname + "!");
 }
