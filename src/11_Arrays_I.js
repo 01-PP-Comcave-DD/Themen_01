bcd9d9e5709348d82972a571132f0fd87ef642a8
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-15 08:41:40 +0000

    array++ | theorie

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,16 +5,16 @@
  *   THEORIE
  * 
  */
-let arr;
-// arr = new Array();           // Konstruktor
-// arr = [];
-arr = [2,5,8,6,11,25,30,8]      // Array mit Elementen (beliebig viel)
+// let arr;
+// // arr = new Array();           // Konstruktor
+// // arr = [];
+// arr = [2,5,8,6,11,25,30,8]      // Array mit Elementen (beliebig viel)
 
-output(arr);
-output(arr.length);             // Anzahl der Elemente
-output(arr[0])                  // Index 0 (0=1) beginnt also bei 0, nicht bei 1
-output(arr[4])                  // Index 4 ist in diesem Beispiel die letzte Zahl(Element)
-output(arr[arr.length-1])       // zeigt nun "automatisch" die letzte zahl an
+// output(arr);
+// output(arr.length);             // Anzahl der Elemente
+// output(arr[0])                  // Index 0 (0=1) beginnt also bei 0, nicht bei 1
+// output(arr[4])                  // Index 4 ist in diesem Beispiel die letzte Zahl(Element)
+// output(arr[arr.length-1])       // zeigt nun "automatisch" die letzte zahl an
 
 
 
@@ -43,6 +43,31 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 
     return str;
 }
+//------------------------------------------------//
+
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
+
 
 
 
