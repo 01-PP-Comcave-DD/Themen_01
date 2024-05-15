455c6a873d286b88bca9d59012baa297338e7665
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-05-15 08:25:04 +0000

    Arrays ++ | 01-Fkt. mit Einzelparameter

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,6 +5,18 @@
  *   THEORIE
  * 
  */
+let arr;
+// arr = new Array();           // Konstruktor
+// arr = [];
+arr = [2,5,8,6,11,25,30,8]      // Array mit Elementen (beliebig viel)
+
+output(arr);
+output(arr.length);             // Anzahl der Elemente
+output(arr[0])                  // Index 0 (0=1) beginnt also bei 0, nicht bei 1
+output(arr[4])                  // Index 4 ist in diesem Beispiel die letzte Zahl(Element)
+output(arr[arr.length-1])       // zeigt nun "automatisch" die letzte zahl an
+
+
 
 
 
@@ -18,13 +30,15 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
